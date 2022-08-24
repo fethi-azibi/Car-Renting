ff851fe7b35f6de1de7c9cc02d44f5977b61ad7c
azibi fethi <azibi.fethi99@gmail.com>
2022-08-24 16:10:02 +0100

    change settings file

--- carRenting/settings.py
+++ carRenting/settings.py
@@ -161,8 +161,8 @@ MESSAGE_TAGS = {
 # email sending
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abdouazibi484@gmail.com'
-EMAIL_HOST_PASSWORD = 'lpunppqmwogerhke'
+EMAIL_HOST_USER = '****@gmail.com'
+EMAIL_HOST_PASSWORD = '****'
 EMAIL_USE_TLS = True
 import _locale
 _locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
